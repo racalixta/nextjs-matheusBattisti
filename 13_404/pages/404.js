1b8c9e9e348439eff01c760695234c8261286f91
racalixta <ra.calixta@gmail.com>
2023-05-30 16:19:21 -0300

    conteudo da aula 15

---
+++ 13_404/pages/404.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function NotFound() {
+
+  return (
+    <>
+      <h1>404</h1>
+      <p>
+        Parece que esta página não existe!
+      </p>
+      <Link href="/"><span>Voltar</span></Link>
+    </>
+  )
+}
