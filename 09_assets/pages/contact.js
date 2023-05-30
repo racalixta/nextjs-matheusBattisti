a72ec3cf61c82f26da20f77fc13236b507eaac97
racalixta <ra.calixta@gmail.com>
2023-05-29 21:49:56 -0300

    conteudo da aula 11

---
+++ 09_assets/pages/contact.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Contact() {
+  return (
+    <>
+      <Head>
+        <title>Contato</title>
+        <meta name="keywords" content='Contato' />
+        <meta name="description" content='Página de Contato' />
+      </Head>
+
+      <div>
+        <h1>Página de Contato</h1>
+      </div>
+    </>
+  )
+}
